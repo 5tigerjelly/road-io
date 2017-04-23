740686787f7ccded22850c21a8ebe73c068c5003
Bum Mook Oh <bmo5@uw.edu>
2017-04-22 18:18:01 -0700

    upload smoke and mirrors

--- website/js/config.js
+++ website/js/config.js
@@ -1,7 +1,7 @@
 var configConstants = {
-    auth0: {
-        domain: 'kriarvi95.auth0.com',
-        clientId: 'AhJgHJqfcPcKEtHDfCt87zC0xffSGajh'
-    },
-    apiBaseUrl: 'https://sejeqwt9og.execute-api.us-west-2.amazonaws.com/Dev'
+    // auth0: {
+    //     domain: 'kriarvi95.auth0.com',
+    //     clientId: 'AhJgHJqfcPcKEtHDfCt87zC0xffSGajh'
+    // },
+    apiBaseUrl: 'https://vpmzqyyafh.execute-api.us-west-2.amazonaws.com/dev'
 };
