228cd09648395435b63aed94ae03360d9408c47f
Arvind <karvi90@yahoo.com>
2017-04-29 22:05:33 -0700

    Carousel

--- LandingPage/js/map.js
+++ LandingPage/js/map.js
@@ -4,7 +4,7 @@ mymap.zoomControl.remove();
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
-    id: 'mapbox.streets'
+    id: 'mapbox.satellite'
 }).addTo(mymap);
 
 mymap.dragging.disable();
@@ -19,18 +19,19 @@ window.onload = function() {
   generateCirclesAfrica();
   generateCirclesSA();
   generateCirclesNA();
+  generateCirclesIndia()
 };
 
 function generateCirclesEurasia() {
     for (i = 0; i < (Math.floor(Math.random() * (220 - 200)) + 220); i++) {
-        var lat = parseFloat(Math.min(37.43997 + (Math.random() * (70.84467 - 37.43997)),70.84467).toFixed(3));
+        var lat = parseFloat(Math.min(37.43997 + (Math.random() * (56.906 - 37.43997)),56.906).toFixed(3));
         var long = parseFloat(Math.min(-8.4375 + (Math.random() * (116.01563 - -8.4375)),116.01563).toFixed(3));
 
-        L.circle([lat, long], 1500, {
+        L.circle([lat, long], 35000, {
             color: 'red',
-            size: 50,
-            fillColor: '#f03',
-            fillOpacity: 0.5
+            size: 5000,
+            fillColor: 'red',
+            fillOpacity: 1
         }).addTo(mymap);
     }
 }
@@ -40,11 +41,11 @@ function generateCirclesAfrica() {
         var lat = parseFloat(Math.min(-18.47961 + (Math.random() * (29.993 - -18.47961)),29.993).toFixed(3));
         var long = parseFloat(Math.min(14.23828 + (Math.random() * (39.02344 - 14.23828)),39.02344).toFixed(3));
 
-        L.circle([lat, long], 1500, {
+        L.circle([lat, long], 35000, {
             color: 'red',
-            size: 50,
-            fillColor: '#f03',
-            fillOpacity: 0.5
+            size: 5000,
+            fillColor: 'red',
+            fillOpacity: 1
         }).addTo(mymap);
     }
 }
@@ -54,11 +55,11 @@ function generateCirclesSA() {
         var lat = parseFloat(Math.min(-18.47961 + (Math.random() * (9.62241 - -18.47961)),9.62241).toFixed(3));
         var long = parseFloat(Math.min(-78.39844 + (Math.random() * (-48.33984 - -78.39844)),-48.33984).toFixed(3));
 
-        L.circle([lat, long], 1500, {
+        L.circle([lat, long], 35000, {
             color: 'red',
-            size: 50,
-            fillColor: '#f03',
-            fillOpacity: 0.5
+            size: 5000,
+            fillColor: 'red',
+            fillOpacity: 1
         }).addTo(mymap);
     }
 }
@@ -68,11 +69,25 @@ function generateCirclesNA() {
         var lat = parseFloat(Math.min(31.65338 + (Math.random() * (52.69636 - 31.65338)),52.69636).toFixed(3));
         var long = parseFloat(Math.min(-85.60547 + (Math.random() * (-76.46484 -  -85.60547)),-76.46484).toFixed(3));
 
-        L.circle([lat, long], 1500, {
+        L.circle([lat, long], 35000, {
             color: 'red',
-            size: 50,
-            fillColor: '#f03',
-            fillOpacity: 0.5
+            size: 5000,
+            fillColor: 'red',
+            fillOpacity: 1
+        }).addTo(mymap);
+    }
+}
+
+function generateCirclesIndia() {
+    for (i = 0; i < (Math.floor(Math.random() * (20 - 10)) + 15); i++) {
+        var lat = parseFloat(Math.min(18.41187 + (Math.random() * (33.66493 - 18.41187)),33.66493).toFixed(3));
+        var long = parseFloat(Math.min(68.55469 + (Math.random() * (106.69922 -  68.55469)),106.69922).toFixed(3));
+
+        L.circle([lat, long], 35000, {
+            color: 'red',
+            size: 5000,
+            fillColor: 'red',
+            fillOpacity: 1
         }).addTo(mymap);
     }
 }
