5b7775ef80449d3d39bb22cbe4de7fa5ec8d9cfb
Josh Hall <jhall38@uw.edu>
2017-05-22 06:14:11 -0700

    efactors dataset history

--- mvp-website/js/confirmed.js
+++ mvp-website/js/confirmed.js
@@ -1,75 +1,13 @@
-var idToken = "";
-var email = "";
-var prefUserName = "";
-var identityID = "";
-var sub = "";
-
-// // Load the SDK for JavaScript
-// $(document).ready(function() {
-//     $('#dataTables-example').DataTable({
-//         responsive: true
-//     });
-// });
-
 $(function() {
-   checkSession();
-   getDatasets();
+  session.checkSession(function(result) {
+    if(!result.loggedIn){
+      window.location.replace("login.html");
+    }
+    else {
+     getDatasets();
+    }
+  });
 });
-
-function checkSession(){
-  var poolData = {
-      UserPoolId : 'us-west-2_dEcrjTcVl',
-      ClientId : '2kkhe3k563aocuioe4sklhokg4'
-  };
-  var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-  var cognitoUser = userPool.getCurrentUser();
-  if (cognitoUser != null) {
-    cognitoUser.getSession(function(err, session) {
-      if (err) {
-          alert(err);
-          return;
-      }
-      idToken = session.idToken.jwtToken;
-      email = cognitoUser.usernmae;
-      cognitoUser.getUserAttributes(function(err, result) {
-        if (err) {
-            alert(err);
-            return;
-        }
-        prefUserName = result[4].getValue();
-        sub = result[0].getValue();
-        $("#userProfileLink").html(prefUserName + "'s profile");
-AWS.config.update({
-  credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-west-2:88b13c2b-9ce8-4370-8071-13f8cd379e01'
-  }),
-  region: 'us-west-2'
-});
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'us-west-2:88b13c2b-9ce8-4370-8071-13f8cd379e01',
-        Logins: {
-          'cognito-idp.us-west-2.amazonaws.com/us-west-2_dEcrjTcVl': session.getIdToken().getJwtToken()
-        }
-      });
-    AWS.config.credentials.refresh((error) => {
-        if (error) {
-            console.error(error);
-        } else {
-            idenittyID = AWS.config.credentials.identityId;
-        }
-        });
-      });
-      $('#signout').click(function(){
-        cognitoUser.signOut();
-        window.location.replace("login.html");
-      });
-    });
-  }
-  else {
-    window.location.replace("login.html");
- 
-  }
-}
 
 function getDatasets() {
   AWS.config.update({accessKeyId: 'AKIAIYWRLAEZKKEYEWSQ', secretAccessKey: 'qbfHk+CLU/yd9hCAPx5a/Nl+8/Ux789hy2WHxsK3', region: 'us-west-2'});
