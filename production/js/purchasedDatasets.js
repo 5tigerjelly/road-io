97b655583ef1101363ecf19a86d3cded0f24f103
Arvind <karvi90@yahoo.com>
2017-05-30 10:55:45 -0700

    Can we redirect to datasetHistory instead?

--- production/js/purchasedDatasets.js
+++ production/js/purchasedDatasets.js
@@ -21,15 +21,17 @@ function loadHistory() {
             if (historicalItems.length > 0) {
                 for (var i = 0; i < historicalItems.length; i++) {
                     let row = $('<tr id="row-' + historicalItems[i].datasetName.split('.')[0] + '"></tr>').appendTo(historicalTable);
-                    $('<td>' + (i + 1) + '</td>').appendTo(row);
-                    $('<td>' + historicalItems[i].datasetName + '</td>').appendTo(row);
-                    $('<td>' + historicalItems[i].purchasedDate + '</td>').appendTo(row);
+                    
+                    var tableID = $("<td></td>").text((i + 1));
+                    
+                    var aTag = $("<a></a>").text(historicalItems[i].datasetName);
+                    aTag.attr("href", "https://s3-us-west-2.amazonaws.com/roadio-datasets/" + historicalItems[i].datasetName);
+                    var newTag = $("<td></td>").html(aTag);
+                    
+                    var newPurchasedDate = $("<td></td>").text(historicalItems[i].purchasedDate);
+                    
+                    row.append(tableID, newTag, newPurchasedDate)
                 }
-                historicalItems.forEach(function(historicalItem) {
-                    let row = $('<tr id="row-' + historicalItem.split('.')[0] + '"></tr>').appendTo(historicalTable);
-                    $('<td>' + counter + '</td>').appendTo(row);
-                    $('<td>' + historicalItem + '</td>').appendTo(row);
-                });
             } else {
                 $('#tableDiv').css('display', 'none');
                 $('#displayNone').append("<h3>You haven't made any purchases!</h3>")
